5cc4778c2ead0aa11696b458a67edb20877ae88c
Elizabeth Martinez <marieli.martinezb@gmail.com>
2022-09-25 21:56:09 -0500

    Paso 1: Creación del servidor express, middlewares

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 8080
+
+const server = app.listen(PORT, () => console.log(`Server up on port ${PORT}`))
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))

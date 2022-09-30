ebeaf642f0710aa2b97fee7d55e561d39835755b
Elizabeth Martinez <marieli.martinezb@gmail.com>
2022-09-29 21:49:09 -0500

    Correcciones en app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
-import express from 'express'
+import express from 'express';
 
-import productRouter from './router/product.router'
-import carritoRouter from './router/carrito.router'
+import { productRouter } from './router/product.router'
+import { carritoRouter } from './router/product.router'
 
 const app = express()
 
 const PORT = process.env.PORT || 8080
 
 const server = app.listen(PORT, () => console.log(`Server up on port ${PORT}`))
+
+server.on('error', (error) => console.log(`Error en server: `, error.message));
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
